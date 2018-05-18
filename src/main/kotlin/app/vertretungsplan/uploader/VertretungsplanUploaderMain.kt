1a2dca988d0bd4fcbd55b620e67a496380ea2f3d
johan12345 <johan.forstner@gmail.com>
2018-05-18 11:36:40 +0200

    tray icon

--- src/main/kotlin/app/vertretungsplan/uploader/VertretungsplanUploaderMain.kt
+++ src/main/kotlin/app/vertretungsplan/uploader/VertretungsplanUploaderMain.kt
@@ -1,18 +1,96 @@
 package app.vertretungsplan.uploader
 
-import app.vertretungsplan.uploader.sync.Sync
 import app.vertretungsplan.uploader.sync.SyncDaemon
 import app.vertretungsplan.uploader.ui.MainView
 import app.vertretungsplan.uploader.ui.style.MainStyleSheet
 import de.codecentric.centerdevice.javafxsvg.SvgImageLoaderFactory
+import it.sauronsoftware.junique.AlreadyLockedException
+import it.sauronsoftware.junique.JUnique
+import it.sauronsoftware.junique.JUnique.acquireLock
+import javafx.application.Platform
+import javafx.beans.property.SimpleObjectProperty
+import javafx.embed.swing.SwingFXUtils
+import javafx.event.EventHandler
 import javafx.scene.image.Image
 import javafx.stage.Stage
-import tornadofx.App
+import javafx.stage.WindowEvent
 import net.harawata.appdirs.AppDirsFactory
+import tornadofx.*
+import java.awt.*
+import java.awt.event.ActionListener
+import java.util.*
+
+
+val APP_ID = "app.vertretungsplan.uploader"
 
 class VertretungsplanUploaderMain : App(MainView::class, MainStyleSheet::class) {
     var stage: Stage? = null
     var daemon: SyncDaemon? = null
+    val trayIcon: TrayIcon
+
+
+    private val _messages: SimpleObjectProperty<ResourceBundle> = object : SimpleObjectProperty<ResourceBundle>() {
+        override fun get(): ResourceBundle? {
+            if (super.get() == null) {
+                try {
+                    val bundle = ResourceBundle.getBundle(this@VertretungsplanUploaderMain.javaClass.name, FX.locale,
+                            this@VertretungsplanUploaderMain.javaClass.classLoader, FXResourceBundleControl)
+                    (bundle as? FXPropertyResourceBundle)?.inheritFromGlobal()
+                    set(bundle)
+                } catch (ex: Exception) {
+                    FX.log.fine("No Messages found for ${javaClass.name} in locale ${FX.locale}, using global bundle")
+                    set(FX.messages)
+                }
+            }
+            return super.get()
+        }
+    }
+
+    var messages: ResourceBundle
+        get() = _messages.get()
+        set(value) = _messages.set(value)
+
+    init {
+        SvgImageLoaderFactory.install();
+        // get the SystemTray instance
+        val tray = SystemTray.getSystemTray()
+        val trayIconSize = tray.trayIconSize
+        // load an image
+        var image = SwingFXUtils.fromFXImage(Image(resources.stream("vertretungsplan_icon.svg"),
+                trayIconSize.width.toDouble(), trayIconSize.height.toDouble(), true, true), null)
+
+        // create a action listener to listen for default action executed on the tray icon
+        val closeListener = ActionListener { System.exit(0) }
+
+        val showListener = ActionListener {
+            Platform.runLater {
+                stage!!.show()
+                stage!!.isIconified = false
+            }
+        }
+        // create a popup menu
+        val popup = PopupMenu()
+
+        val showItem = MenuItem(messages["settings"])
+        showItem.addActionListener(showListener)
+        popup.add(showItem)
+
+        val closeItem = MenuItem(messages["exit"])
+        closeItem.addActionListener(closeListener)
+        popup.add(closeItem)
+        /// ... add other items
+        // construct a TrayIcon
+        trayIcon = TrayIcon(image!!, messages["app_name"], popup)
+        // set the TrayIcon properties
+        trayIcon.addActionListener(showListener)
+        // ...
+        // add the tray image
+        try {
+            tray.add(trayIcon)
+        } catch (e: AWTException) {
+            System.err.println(e)
+        }
+    }
 
     private val appDirs = AppDirsFactory.getInstance()!!
     // Keep version argument at 1, we do not want new folders for every new version for now.
@@ -20,36 +98,12 @@ class VertretungsplanUploaderMain : App(MainView::class, MainStyleSheet::class) 
     val configStore = VertretungsplanUploaderPrefs(dataDir)
 
     override fun start(stage: Stage) {
-        SvgImageLoaderFactory.install();
         this.stage = stage
 
-        /*try {
-            acquireLock(APP_ID, fun (message: String): String {
-                fire(ConfigureEvent(message))
-                return "ok"
-            })
-        } catch (e: AlreadyLockedException) {
-            if (getInitUrl() != null) {
-                JUnique.sendMessage(APP_ID, getInitUrl())
-                System.exit(0)
-            } else {
-                val alert = Alert(AlertType.INFORMATION)
-                alert.title = messages["running_already_title"]
-                alert.headerText = messages["running_already_title"]
-                alert.contentText = messages["running_already"]
+        setupCloseToTray(stage)
+        ensureSingleInstance(stage)
 
-                val buttonTypeCancel = ButtonType(messages["alert_cancel"], ButtonBar.ButtonData.OK_DONE)
-                val buttonTypeIgnore = ButtonType(messages["alert_ignore"], ButtonBar.ButtonData.OTHER)
-                alert.buttonTypes.setAll(buttonTypeIgnore, buttonTypeCancel)
-
-                val res = alert.showAndWait()
-                if (res.get() == buttonTypeCancel) {
-                    System.exit(1)
-                }
-            }
-        }*/
-
-        //stage.icons += Image(VertretungsplanUploaderMain::class.java.getResourceAsStream("icon.png"))
+        stage.icons += Image(resources.stream("vertretungsplan_icon.svg"))
         stage.minHeight = 100.0
         stage.minWidth = 800.0
 
@@ -60,6 +114,38 @@ class VertretungsplanUploaderMain : App(MainView::class, MainStyleSheet::class) 
         stylesheets.addAll(resources["/css/jfoenix-fonts.css"], resources["/css/jfoenix-design.css"])
 
         daemon!!.start()
+    }
+
+    private fun setupCloseToTray(stage: Stage) {
+        if (SystemTray.isSupported()) {
+            stage.onCloseRequest = EventHandler<WindowEvent> {
+                Platform.runLater {
+                    if (SystemTray.isSupported()) {
+                        stage.hide()
+                        //showProgramIsMinimizedMsg()
+                    } else {
+                        System.exit(0)
+                    }
+                }
+            }
+
+        }
+        Platform.setImplicitExit(false);
+    }
+
+    private fun ensureSingleInstance(stage: Stage) {
+        try {
+            acquireLock(APP_ID, fun(message: String): String {
+                Platform.runLater {
+                    stage.show()
+                    stage.isIconified = false
+                }
+                return "ok"
+            })
+        } catch (e: AlreadyLockedException) {
+            JUnique.sendMessage(APP_ID, "")
+            System.exit(0)
+        }
     }
 
     fun restartDaemon() {
